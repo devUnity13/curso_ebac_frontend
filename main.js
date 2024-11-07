2e4378cdcd0f44b1000351bc1a242c16851b20f8
devUnity13 <carlosfreitas.dev117@gmail.com>
2024-11-07 03:23:12 -0300

    exercicio html js validaçao

--- main.js
+++ main.js
@@ -4,25 +4,35 @@ let formValido = false
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    let numeroA = parseInt(document.getElementById('numero-a').value);
-    let numeroB = parseInt(document.getElementById('numero-b').value);
+    let numeroA = document.getElementById('numero-a');
+    let numeroB = document.getElementById('numero-b');
 
     console.log(typeof (numeroA))
     console.log(typeof (numeroB))
 
-    formValido = numeroB > numeroA
+    formValido = parseInt(numeroB.value) > parseInt(numeroA.value)
 
     const mensagemDisplaySucess = document.querySelector('.message-success')
     const mensagemDisplayError = document.querySelector('.message-error')
-    const mensagemSucesso = `numero B: ${numeroB} é maior que o número A ${numeroA}`
-    const mensagemErro = `numero B: ${numeroB} é menor que o número A: ${numeroA}`
+
+    const mensagemSucesso = `numero B: ${numeroB.value} é maior que o número A ${numeroA.value}`
+    const mensagemErro = `numero B: ${numeroB.value} é menor que o número A: ${numeroA.value}`
 
     if (formValido) {
         mensagemDisplaySucess.innerHTML = mensagemSucesso
         mensagemDisplaySucess.style.display = 'block'
 
         document.getElementById('numero-b').classList.remove('error')
+        document.getElementById('numero-a').classList.remove('error')
         mensagemDisplayError.style = ''
+    }
+
+    //aqui eu pensei 0 é menor do que qualquer numero positivo mas, é maior do que qualquer numero negativo.
+    else if (parseInt(numeroB.value) === parseInt(numeroA.value)) {
+        mensagemDisplayError.innerHTML = `Não permitimos valores iguais!`;
+        mensagemDisplayError.style.display = 'block'
+        document.getElementById('numero-b').classList.add('error')
+        document.getElementById('numero-a').classList.add('error')
     }
     else {
         mensagemDisplayError.innerHTML = mensagemErro;
@@ -31,4 +41,7 @@ form.addEventListener('submit', (event) => {
 
         mensagemDisplaySucess.style = ''
     }
+
+    numeroA.value = ''
+    numeroB.value = ''
 })
